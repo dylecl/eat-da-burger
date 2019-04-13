828f3e090cdf95e677b8b9ae2198244aaab2c656
dylecl <dylann.leclairr@gmail.com>
2019-04-12 20:57:18 -0600

    maybe now

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
     host: "127.0.0.1",
-    port: 8889,
+    port: 3306,
     user: "root",
     password: "root",
     database: "burger_db2"
